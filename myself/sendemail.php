3ad21a2ee1156789744128724d74026e51a07a15
shuuki4 <123bskim@naver.com>
2016-05-17 02:01:35 +0900

    modifying CV

--- myself/sendemail.php
+++ myself/sendemail.php
@@ -6,7 +6,7 @@
     header( 'Content-type: application/json' );
 
 
-    $to         = 'trendytheme.net@gmail.com';  // put your email here
+    $to         = '123bskim@naver.com';  // put your email here
 
     $email_template = 'simple.html';
 
